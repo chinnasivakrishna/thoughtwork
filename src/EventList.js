b0489f960edef08cc249c9c54b2c170cea741adb
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-09-18 18:39:35 +0530

    update

--- src/EventList.js
+++ src/EventList.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const EventList = ({ events, deleteEvent }) => {
+const EventList = ({ events, onEditEvent, onDeleteEvent }) => {
   return (
     <div>
-      <h2>Upcoming Events</h2>
-      <ul>
-        {events.map(event => (
-          <li key={event.id}>
-            <h3>{event.name}</h3>
-            <p>{event.date} at {event.time}</p>
-            <p>{event.description}</p>
-            <button onClick={() => deleteEvent(event.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {events.map((event) => (
+        <div key={event.id} className="event-item">
+          <h3>{event.name}</h3>
+          <p>{event.date} at {event.time}</p>
+          <p>{event.description}</p>
+          <button onClick={() => onEditEvent(event)}>Edit</button>
+          <button onClick={() => onDeleteEvent(event.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 };
